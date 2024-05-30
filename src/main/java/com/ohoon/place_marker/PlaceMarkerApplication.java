f1651f9105e8d5d34d319710102fd9df73e0715e
0hoon5661 <0hoon5661@gmail.com>
2024-05-30 17:52:44 +0900

    start spring io

---
+++ src/main/java/com/ohoon/place_marker/PlaceMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.ohoon.place_marker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceMarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohoon/place_marker/PlaceMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohoon.place_marker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
